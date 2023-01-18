6d7702f9d78b4ce9efa5702decbf63c450bdc657
William Chalk <william@printedproducts.com>
2023-01-18 16:57:12 -0500

    added handleChange admin form

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import {Container,Form,Button} from "react-bootstrap";
-
+import { useMutation } from "@apollo/client";
+import { ADD_ANNOUNCEMENT,ADD_LEAGUE,ADD_PLAYERS } from "../graphql/mutations";
 
 import Auth from "../context/auth";
 
 function Admin(){
+
+    const [formState,setFormState] = useState({title:"",content:"",leagueName:"",firstname:"",lastname:"",playerPosition:"",playerNumber:""});
+    const [addAnnouncement] = useMutation(ADD_ANNOUNCEMENT);
+    const [addLeague] = useMutation(ADD_LEAGUE);
+    const [addPlayer] = useMutation(ADD_PLAYERS);
+
+    const handleChange=(event)=>{
+        const {name,value} = event.target;
+
+        setFormState({
+            ...formState,
+            [name]:value,
+        });
+    };
+
+
 
     return(
         <div>
@@ -15,11 +32,11 @@ function Admin(){
                 <Form>
                     <Form.Group className="mb-3" controlId="announcementTitle">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" placeholder="Title"/>
+                        <Form.Control type="text" placeholder="Title" name="title" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="announcementContent">
                         <Form.Label>Content</Form.Label>
-                        <Form.Control as="textarea" rows={3}/>
+                        <Form.Control as="textarea" rows={3} name="content" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="announcementImage">
                         <Form.Label>Add Image</Form.Label>
@@ -33,7 +50,7 @@ function Admin(){
                 <Form>
                     <Form.Group className="mb-3" controlId="leagueName">
                         <Form.Label>League Name</Form.Label>
-                        <Form.Control type="text" placeholder="League Name"/>
+                        <Form.Control type="text" placeholder="League Name" name="leagueName" onChange={handleChange}/>
                     </Form.Group>
                     <Button type="submit" variant="success">Add League</Button>
                 </Form>
@@ -43,19 +60,19 @@ function Admin(){
                 <Form>
                     <Form.Group className="mb-3" controlId="teamMemberFirstName">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Name"/>
+                        <Form.Control type="text" placeholder="First Name" name="firstname" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="teamMemberLastName">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Last Name"/>
+                        <Form.Control type="text" placeholder="Last Name" name="lastname" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="teamMemberPosition">
                         <Form.Label>Position</Form.Label>
-                        <Form.Control type="text" placeholder="Player Position"/>
+                        <Form.Control type="text" placeholder="Player Position" name="playerPosition" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="teamMemberNumber">
                         <Form.Label>Player Number</Form.Label>
-                        <Form.Control type="number" placeholder="Player Number"/>
+                        <Form.Control type="number" placeholder="Player Number" name="playerNumber" onChange={handleChange}/>
                     </Form.Group>
                 </Form>
                 </>
