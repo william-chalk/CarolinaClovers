af8f932570fd032111819b72deca77a73136c72f
William Chalk <william@printedproducts.com>
2023-01-23 16:40:08 -0500

    testing user role authentication

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -11,6 +11,18 @@ class AuthService {
     return !!token && !this.isTokenExpired(token); // handwaiving here
   }
 
+  isAdmin(){
+      const token = this.getToken();
+      const decoded = decode(token);
+
+      if(loggedIn() && decoded.role === "admin"){
+        return true;
+      }
+
+      return false;
+    
+  }
+
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
